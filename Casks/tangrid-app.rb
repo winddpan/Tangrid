a305db0583c55ba77b031406631f3316e8bbcb47
winddpan <winddpan@gmail.com>
2026-08-13 09:25:49 +0800

    cask tangrid-app

--- Casks/tangrid-app.rb
+++ Casks/tangrid-app.rb
@@ -1,10 +1,10 @@
-cask "tangrid" do
+cask "tangrid-app" do
   version "1.5.8"
   sha256 "584c7a1aaaba5eddbc7b3df774fd3fde5de60a67540b29c3957b4093b8845e0b"
 
   url "https://github.com/winddpan/Tangrid/releases/download/1.5.8/tangrid-1.5.8.zip"
   name "Tangrid"
-  desc "Window manager with snapping, tiling, and Winodw Switcher, Dock previews, Workspace"
+  desc "Window manager with snapping, tiling, Window Switcher, Dock previews, Workspace"
   homepage "https://github.com/winddpan/Tangrid"
 
   livecheck do
